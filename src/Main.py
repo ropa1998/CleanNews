48285b14104e248843efd1893ec2df048c129528
rodrigo <ropa1998@gmail.com>
2019-02-28 16:39:26 -0300

    the article elements are being retrieved, yet i could only retireve 10 of it, no more.

--- src/Main.py
+++ src/Main.py
@@ -7,7 +7,8 @@ browser = getBrowser_Firefox(invisible_window=False)
 REGIONS = getAutomaticRegions()
 
 regions_with_trends = TrendRetriever.getTrends_Trends24(REGIONS, browser)
-news = NewsRetriever.TrendSearch_Google(regions_with_trends, browser)
+news = NewsRetriever.TrendSearchPerRegionThroughSpecificMedia(regions_with_trends, browser)
+
 
 
 browser.quit()
--- src/NewsRetriever.py
+++ src/NewsRetriever.py
@@ -1,11 +1,10 @@
 import time
 
 from selenium.webdriver.common.keys import Keys
-
+from selenium.webdriver.remote.webelement import WebElement
 
 
 def TrendSearch_Google(regions, browser):
-
     for region in regions:
         for trend in region.getTrends():
             browser.get('http://www.google.com')
@@ -15,6 +14,11 @@ def TrendSearch_Google(regions, browser):
             time.sleep(5)
 
 
-
-
-
+def TrendSearchPerRegionThroughSpecificMedia(regions, browser):
+    for region in regions:
+        for medium in region.getMedia():
+            for trend in region.getTrends():
+                browser.get(medium)
+                articles = browser.find_elements_by_tag_name('article')
+                for article in articles:
+                    print article.text
